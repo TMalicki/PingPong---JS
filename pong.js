dae06226d0c326ea7c0f761e56d4c8c4f7633cd6
Tomasz Malicki <t.malicki1@wp.pl>
2020-07-19 16:24:39 +0200

    Add files via upload

--- pong.js
+++ pong.js
@@ -1,24 +1,46 @@
+// should use continuous collision detection (not discrete)
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 
 const height = 500;
 const width = 1000;
+const fieldLineWidth = 2;
+const fieldLineHeight = 20;
 canvas.height = height;
 canvas.width = width;
 
 const ballSize = 20;
-let ballPosX = width/2 - ballSize/2;
-let ballPosY = height/2 - ballSize/2;
-let ballSpeedX = 0.5;
-let ballSpeedY = 0.5;
+let ballPosX = width/2;
+let ballPosY = height/2;
+let ballSpeedX = -5;
+let ballSpeedY = -5;
 
-const playerHeight = 100;
-const playerWidth = 20;
-const fieldLineWidth = 2;
-const fieldLineHeight = 20;
+const racketHeight = 100;
+const racketWidth = 20;
+
+let playerPosY = height/2;
+let aiPosY = height/2;
+
 let mousePosY = 0;
-let playerPosY = height/2 - playerHeight/2;
 let collision = false;
+
+let ballUpBorder = ballPosY - ballSize/2;
+let ballDownBorder = ballPosY + ballSize/2;
+let ballLeftBorder = ballPosX - ballSize/2;
+let ballRightBorder = ballPosX + ballSize/2;
+
+let playerUpBorder = playerPosY - racketHeight/2;
+let playerDownBorder = playerPosY + racketHeight/2;
+let playerLeftBorder = 70;
+let playerRightBorder = 70 + racketWidth;
+let aiLeftBorder = 930 - racketWidth;
+let aiRightBorder = 930;
+
+let lastError = 0; 
+let iError = 0; 
+let dError = 0; 
+let pError = 0;
 
 function setup(width, height)
 {
@@ -34,13 +56,11 @@ function setup(width, height)
 
 function updateBall()
 {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(ballPosX, ballPosY, ballSize, ballSize);
-
     ballPosX += ballSpeedX;
     ballPosY += ballSpeedY;
 
     collisionDetection();
+    
     /*
     if(collision)
     {
@@ -65,48 +85,124 @@ function updateBall()
 
 function collisionDetection()
 {
-    if(ballPosY + ballSize >= height || ballPosY <= 0) 
+    borderCollision();
+    racketCollision();
+}
+
+function borderCollision()
+{
+    if(ballPosY + ballSize/2 >= height || ballPosY - ballSize/2 <= 0) 
     {
         ballSpeedY = -ballSpeedY;
         collision = true;
     }
-    else if(ballPosX + ballSize >= width || ballPosX <= 0) 
+    else if(ballPosX + ballSize/2 >= width || ballPosX - ballSize/2 <= 0) 
     {
         ballSpeedX = -ballSpeedX;
         collision = true;
     }   
 }
 
-function updatePlayer()
+function AABBintersects(differenceValues)
+{
+    if(differenceValues[2] > 0.0 || differenceValues[1] > 0.0) return false;
+    if(differenceValues[0] > 0.0 || differenceValues[3] > 0.0) return false;
+
+    return true;
+}
+
+function racketCollision()
+{
+    ballUpBorder = ballPosY - ballSize/2;
+    ballDownBorder = ballPosY + ballSize/2;
+    ballLeftBorder = ballPosX - ballSize/2;
+    ballRightBorder = ballPosX + ballSize/2;
+
+    playerUpBorder = playerPosY - racketHeight/2;
+    playerDownBorder = playerPosY + racketHeight/2;
+
+    let depthCollision = 0.0;
+
+    let differenceValues = [playerUpBorder - ballDownBorder,    //playerUpBallDown
+                            ballUpBorder - playerDownBorder,    //playerDownBallUp
+                            ballLeftBorder - playerRightBorder, //playerRightBallLeft
+                            playerLeftBorder - ballRightBorder] //playerLeftBallRight
+
+    if(AABBintersects(differenceValues)) 
+    {
+        let calculatedDepth = Math.max.apply(null,differenceValues);
+
+        if(depthCollision >= calculatedDepth) depthCollision = calculatedDepth;
+
+        if(depthCollision == differenceValues[0] || depthCollision == differenceValues[1]) ballSpeedY = -ballSpeedY;
+        else if(depthCollision == differenceValues[2] || depthCollision == differenceValues[3]) ballSpeedX = -ballSpeedX;
+    }
+}
+
+function drawBall()
 {
     ctx.fillStyle = 'white';
-    ctx.fillRect(70, playerPosY, playerWidth, playerHeight);
- 
+    ctx.fillRect(ballPosX - ballSize/2, ballPosY - ballSize/2, ballSize, ballSize);
+}
+
+function drawPlayer()
+{
+    ctx.fillStyle = 'white';
+    ctx.fillRect(70, playerPosY - racketHeight/2, racketWidth, racketHeight);
+}
+
+function drawAI()
+{
+    ctx.fillStyle = 'white';
+    ctx.fillRect(width - 70 - racketWidth, aiPosY - racketHeight/2, racketWidth, racketHeight);
+}
+
+function updatePlayer(e)
+{
+    mousePosY = e.clientY - canvas.offsetTop;
+    playerPosY = mousePosY;
+    
+    if(playerPosY - racketHeight/2 < 0) playerPosY = racketHeight/2; 
+    else if(playerPosY + racketHeight/2 > height) playerPosY = height - racketHeight/2;
 }
 
 function updateAI()
 {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(width - 70 - playerWidth, height/2 - playerHeight/2, playerWidth, playerHeight);
+    if(ballPosX < width/2)
+    {
+        aiPID(0.02, 50000, 100)//2000, 1000
+    }
+    else
+    {
+        aiPID(0.1, 10000, 1000)//2000, 1000
+    }
 }
 
-function playerPosition(e)
+function aiPID(kp, Ti, Kd)
 {
-    mousePosY = e.clientY - canvas.offsetTop;
-    playerPosY = mousePosY - playerHeight/2;
-    
-    if(playerPosY < 0) playerPosY = 0; 
-    else if(playerPosY > height - playerHeight) playerPosY = height - playerHeight;
+    debugger;
+    pError = (ballPosY - aiPosY);
+    iError += pError * 1000/60;
+    dError = (pError - lastError) / (1000 * 60);
+
+    aiPosY += kp * pError + (1/Ti) * (iError) - Kd * dError;
+
+    if(aiPosY - racketHeight/2 < 0) aiPosY = racketHeight/2; 
+    else if(aiPosY + racketHeight/2 > height) aiPosY = height - racketHeight/2;
+    lastError = pError;
+    debugger;
 }
 
 function game()
 {
     setup(width, height);
     updateBall();
-    updatePlayer();
-    updateAI(); 
+    updateAI();
+    drawPlayer();
+    drawAI(); 
+    drawBall();
 }
 
-canvas.addEventListener("mousemove", function(e){ playerPosition(e) }, false);
+canvas.addEventListener("mousemove", function(e){ updatePlayer(e) }, false);
 
-window.setInterval(game, 0.2);
+window.setInterval(game, 1000/60);
